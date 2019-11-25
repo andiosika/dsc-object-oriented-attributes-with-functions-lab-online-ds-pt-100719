820c680d00d4a21beeffd16cd6203b5eb7eb55e4
andiosika <andiosika@gmail.com>
2019-11-25 10:40:22 -0700

    need to finish this one

--- school.py
+++ school.py
@@ -1,1 +1,44 @@
-class School:
+class School():
+	def __init__(self, school, roster={full_name, grade}):
+		self.school = school
+		self.roster = roster
+	def add_student(self, full_name, grade):
+		add_student.full_name = full_name
+		add_student.grade = grade
+		add_student.append.roster(Student)
+	def Grade(self, grade):
+		grade.students = roster.get[grade(full_name)]
+	return students
+
+	def sort_roster(self, Grade):
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		
+		
+
+
+	
+	
+	
+	
+
+	
+
