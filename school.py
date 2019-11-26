bd6763267431c4881904249c5b20b34f56bcf13f
andiosika <andiosika@gmail.com>
2019-11-26 11:07:38 -0700

    worked through lab

--- school.py
+++ school.py
@@ -1,16 +1,19 @@
-class School():
-	def __init__(self, school, roster={full_name, grade}):
-		self.school = school
-		self.roster = roster
-	def add_student(self, full_name, grade):
-		add_student.full_name = full_name
-		add_student.grade = grade
-		add_student.append.roster(Student)
-	def Grade(self, grade):
-		grade.students = roster.get[grade(full_name)]
-	return students
+class School:
+    def __init__(self, school, roster={}):
+        self.school = school
+        self.roster = roster
+    def add_student(self, full_name=None, grade=None):
+        self.full_name = full_name
+        self.grade = grade
+        self.roster.update({full_name : grade})
+    def Grade(self, grade=int):
+        self.grade = grade
+        return self.roster[grade]
+    def sort_roster(self):
+        self.sort_roster = sort_roster
+        return self.roster.items.sorted()
+        
 
-	def sort_roster(self, Grade):
 	
 
 
