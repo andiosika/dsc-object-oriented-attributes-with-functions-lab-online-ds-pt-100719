9934b697d8a2dcb4c2b3c4ab1300d6513b079572
andiosika <andiosika@gmail.com>
2019-12-04 07:54:38 -0700

    still have none error for sort function

--- school.py
+++ school.py
@@ -1,34 +1,20 @@
 class School:
-<<<<<<< HEAD
-    def __init__(self, school, roster={}):
+    def __init__(self, school=None):
         self.school = school
-        self.roster = roster
+        self.roster = {}
     def add_student(self, full_name=None, grade=None):
         self.full_name = full_name
         self.grade = grade
-        self.roster.update({full_name : grade})
-    def Grade(self, grade=int):
+        if grade in self.roster: 
+            self.roster[grade].append(full_name)
+        else:
+            self.roster[grade] = [full_name]
+    def Grade(self, grade):
         self.grade = grade
         return self.roster[grade]
     def sort_roster(self):
-        self.sort_roster = sort_roster
-        return self.roster.items.sorted()
-        
-
-=======
-	def __init__(self, school, roster={full_name, grade}):
-		self.school = school
-		self.roster = roster
-	def add_student(self, full_name, grade):
-		add_student.full_name = full_name
-		add_student.grade = grade
-		add_student.append.roster(Student)
-	def Grade(self, grade):
-		grade.students = roster.get[grade(full_name)]
-	return students
-
-	def sort_roster(self, Grade):
->>>>>>> 697cb72779522d4bfb44b5b9d3525f9aa8c149eb
+        for i in self.roster.keys():
+            self.roster[i]=sorted(self.roster[i])
 	
 
 
